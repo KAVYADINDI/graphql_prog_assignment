a36af1fce8aaf17ab55ca508706c06e706c010a0
kavyadindi <kavya.kalidindi@gmail.com>
2025-07-26 19:37:19 -0500

    added logging for server URL

--- index.js
+++ index.js
@@ -107,7 +107,8 @@ mongoose.connect(MONGODB, {
 
         const PORT = process.env.PORT || 4000;
         app.listen(PORT, () => {
-            console.log(`Server is running on http://localhost:${PORT}${server.graphqlPath}`);
+            console.log(`GraphQLServer is running on http://localhost:${PORT}${server.graphqlPath}`);
+            console.log(`Server is running on http://localhost:${PORT}`);
         }); 
         })
     .catch(err => {
